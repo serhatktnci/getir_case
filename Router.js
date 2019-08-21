ea3871f374a52d55cb9b2edbe61815b649bbf165
Serhat Ketenci <serhat.ketenci0@gmail.com>
2019-08-21 19:39:18 +0300

    Router.js refactoring. add method documentation and first page(get) add git repo info

--- Router.js
+++ Router.js
@@ -3,9 +3,22 @@ const recordController = require('./controllers/recordController'),
 
 
 module.exports = function (app) {
-    const apiRoutes = express.Router();
+const apiRoutes = express.Router();
 
-    apiRoutes.post('/getRecords', recordController.getRecords);
-    app.use('/',apiRoutes);
+
+// @route Post /getRecords
+// @desc getRecords params{startDate,endDate,minCount,maxCount}
+// @access Public
+apiRoutes.post('/getRecords', recordController.getRecords);
+
+
+// @route Get /
+// @desc Create a Todo Item
+// @access Public
+apiRoutes.get('/', function(req, res){
+  res.send('Welcome. <a href="https://github.com/serhatktnci/getir_case">Github Repo</a>');
+});
+
+app.use('/',apiRoutes);
 
 };
